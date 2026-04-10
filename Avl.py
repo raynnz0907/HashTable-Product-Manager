bb4e948b58099026c6bdfb5c7298c04964a77e87
raynnz0907 <rayan310807@gmail.com>
2026-04-10 21:14:28 +0530

    Insertion function

--- Avl.py
+++ Avl.py
@@ -11,3 +11,22 @@ class HashTable:
     def rehash(self, index):
         return (index + 1) % self.size
 
+    # Insert product
+    def insert(self, key, value):
+        index = self.hash_function(key)
+
+        original_index = index
+        while self.table[index] is not None:
+            # If key already exists, update value
+            if self.table[index][0] == key:
+                self.table[index] = (key, value)
+                return
+            index = self.rehash(index)
+
+            if index == original_index:
+                print("Hash Table is full!")
+                return
+
+        self.table[index] = (key, value)
+        print(f"Product '{key}' added successfully!")
+
